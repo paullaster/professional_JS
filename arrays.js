71c10424ecc30281c19a666fab6b4d76f0954058
paullaster <paullasterokoth98@gmail.com>
2022-09-14 22:54:17 +0300

    getting last occurence

--- arrays.js
+++ arrays.js
@@ -4,5 +4,6 @@ list1.push(...list2)
 list1.splice([list1.length], 0, 20,40,60, 20);
 let find1 = list1.find((item)=> item === 20);
 let findByIndex = list1.indexOf(20, 11);
+let lastOccurrence = list1.lastIndexOf(20);
 
-console.table(findByIndex);
+console.table(lastOccurrence);
