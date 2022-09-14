ecc8362a5c959c0e7d1cbd0cb205eebed81c227a
paullaster <paullasterokoth98@gmail.com>
2022-09-14 22:31:52 +0300

    adding splice

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
 let list1 = [1,2,3,4,5];
 let list2 = [6,7,8,9,10];
-console.log(list1.push(...list2), list1)
+list1.push(...list2)
+list1.splice([list1.length], 0, 20,40,60);
+console.table(list1);
