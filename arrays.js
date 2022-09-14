525a8ede9008ac7da1d51c761d1af45dda67bdb9
paullaster <paullasterokoth98@gmail.com>
2022-09-14 22:45:21 +0300

    array.find

--- arrays.js
+++ arrays.js
@@ -2,4 +2,5 @@ let list1 = [1,2,3,4,5];
 let list2 = [6,7,8,9,10];
 list1.push(...list2)
 list1.splice([list1.length], 0, 20,40,60);
-console.table(list1);
+let find1 = list1.find((item)=> item === 30);
+console.table(find1);
