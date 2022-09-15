7e44393915fd39a72ce0ab9cb82866dafb246420
paullaster <paullasterokoth98@gmail.com>
2022-09-15 08:48:07 +0300

    switch playg-

--- conditional.js
+++ conditional.js
@@ -12,7 +12,35 @@ if(age > 21){
 }*/
 //console.log(typeof age);
 //
-let ID = "invalid";
+/*let ID = "invalid";
 ID ==="valid" ? 
 console.log("Allowed entry to the venue")
 : console.log("Denied access to the venue");
+*/
+
+let randonNum = Math.floor(Math.random() *6);
+let usrInput = prompt("say something");
+switch (randonNum){
+    case 0:
+        console.log(randonNum + " zero");
+        break;
+    case 1:
+        console.log(randonNum + " one");
+        break;
+    case 2:
+        console.log(randonNum + " two");
+        break;
+    case 3:
+        console.log(randonNum + " three");
+        break;
+    case 4:
+        console.log(randonNum + " four");
+        break;
+    case 5:
+        console.log(randonNum + " five");
+        break;
+    default:
+        console.log(randonNum + " unknown");
+        break;
+}
+//console.log(randonNum, usrInput);
