3647e18d71693a25a18a0d02a835373d4adc9ad6
paullaster <paullasterokoth98@gmail.com>
2022-09-15 08:07:27 +0300

    condition playground

--- conditional.js
+++ conditional.js
@@ -1,1 +1,14 @@
-print("I love you")
+let age = parseInt(prompt("Enter your age"));
+let message = "You are denied entry"
+if(age > 21){
+    message ="You are allowed entry to the venue and you can also buy alcohol";
+    console.log(message);
+}else if(age > 19){
+    message ="You are allowed entry to the venue";
+    console.log(message);
+}else{
+    message;
+    console.log(message);
+}
+//console.log(typeof age);
+//console.time(age);
