c5256c4f09d0afc59451fbcce45923b057494920
paullaster <paullasterokoth98@gmail.com>
2022-09-16 19:25:31 +0300

    adding table data

--- excel.table.js
+++ excel.table.js
@@ -1,7 +1,8 @@
 /**
  * MOCK EXCEL OR GOOGLE DOCS
  */
-let divContainer = document.getElementsByTagName("div");
+let divContainer = document.querySelector(divContainerFunction());
+let tablerow;
 let table = [];
 let colSize = 26;
 let rowSize = 1000;
@@ -16,9 +17,9 @@ for(let i = 0; i <rowSize; i++) {
 }
 
 let pageTable = `
-    <table>
+    <table colspan="2">
         <tr>
-            <thead>
+            <thead colspan="2">
                 ${
                  (
                     ()=>{
@@ -30,14 +31,27 @@ let pageTable = `
                 }
             </thead>
         </tr>
+        <tr>
+            <tbody>
+            ${
+                (
+                    ()=>{
+                        let tableData;
+                        for(let i = 0; i < colSize; i++) {
+                            for(let j = 0; j < rowSize; j++){
+                            return tableData = `<td>${table[i][j]}</td>`;
+                            }
+                        }
+                    }
+                )()
+            }
+            </tbody>
+        </tr>
     </table>
 `;
-//console.log(divContainer);
-//divContainer.insertAdjacentHTML('beforeEnd', pageTable); 
-const h2 = document.getElementById("myH2");
-let html = "<p>My new paragraph.</p>";
-h2.insertAdjacentHTML("afterend", html);
-function newFunction() {
-    return "myH2";
-}
+console.log(divContainer);
+divContainer.insertAdjacentHTML("beforeend", pageTable);
+function divContainerFunction() {
+    return "#container";
+};
 
