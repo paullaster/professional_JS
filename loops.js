952f7c53859d0329ab8ecb6adb95e7ffb2171bdc
paullaster <paullasterokoth98@gmail.com>
2022-09-16 09:24:17 +0300

    creating guessArray

--- loops.js
+++ loops.js
@@ -38,10 +38,25 @@ while(fibArray.length < 25){
  */
 
 let maxGuess = 10;
+let minGuess = 0;
 let guessArray = [];
 let randArray = [];
+let counter = 0;
 while(randArray.length < maxGuess){
     let randArrayItems = Math.floor(Math.random() * maxGuess) + 1;
     randArray = [...randArray, randArrayItems];
 }
-console.table(randArray);
+while(guessArray.length < maxGuess){
+    let playerGuess = parseInt(prompt("Enter your guess number : Between "
+     +minGuess +  " and "+maxGuess + "\n  You have played " + counter + " rounds." ));
+    if(playerGuess > maxGuess){
+        alert(playerGuess + " is too big. Please try again. By choosing between "+maxGuess+" and "+minGuess+".");
+        break;
+    }
+    guessArray = [...guessArray, playerGuess];
+    counter++;
+}
+for(let items of guessArray){
+    console.log(items);
+}
+//console.table(guessArray);
