82ca83118c62d2bfec6aa2ee6ac69a7fbd28ad51
paullaster <paullasterokoth98@gmail.com>
2022-09-17 17:38:27 +0300

    correct bigvalue func

--- func_string_gen.js
+++ func_string_gen.js
@@ -10,16 +10,12 @@ function stringGenerator(vala,valb,valc) {
 }
 
 function bigValue(a,b,c){
-    if(a < b ){
-        if( c < b ){
-            console.log(b + ' is biggest');
-        }
-    }else{
-        if(a < c){
+    if(a < b && b < c){
+        if( a < c ){
             console.log(c + ' is biggest');
-        }else{
-            console.log(a + ' is biggest');
         }
+        console.log(a + ' is biggest');
     }
+    console.log(b + ' is biggest');
 }
-stringGenerator(12,12,2);
+stringGenerator(2,10,3);
