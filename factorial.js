af0403c210bf70559660a9903d42f792240ef9d1
paullaster <paullasterokoth98@gmail.com>
2022-09-20 05:57:23 +0300

    adding factorial func

--- factorial.js
+++ factorial.js
@@ -1,3 +1,13 @@
 /**
  * Function for finding factorial of numbers
  */
+
+function factorial(n) {
+    let facto = n;
+    console.log(facto);
+    if (n > 1 ){
+        factorial(n - 1);
+    }
+}
+
+factorial(5);
