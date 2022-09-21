f84a4d78118c07fd1417d25ae9c67b7bf88c4882
paullaster <paullasterokoth98@gmail.com>
2022-09-21 05:01:52 +0300

    .

--- async_funct.js
+++ async_funct.js
@@ -4,8 +4,16 @@
 
 function one () {
     console.log("one");
-}
+};
+
+function two () {
+    console.log("two");
+};
 
 function three () {
     console.log("three");
-}
+};
+
+function four () {
+    console.log("four");
+};
