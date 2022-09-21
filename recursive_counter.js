8fd0745222759b249a3d256ef9baeb91887f98ff
paullaster <paullasterokoth98@gmail.com>
2022-09-21 04:46:52 +0300

    recursive counter

---
+++ recursive_counter.js
@@ -0,0 +1,10 @@
+/**
+ * Recursive funtions that counts based on the dynamic number
+ */
+
+function dynamicCounter (value) {
+    console.log(value);
+    if (value === 0) {
+        return 1;
+    }
+}
