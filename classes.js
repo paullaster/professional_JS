b8b581aa51be02230013022829c9552b367015e9
paullaster <paullasterokoth98@gmail.com>
2022-09-21 09:55:02 +0300

    .

--- classes.js
+++ classes.js
@@ -2,12 +2,16 @@
  * Classes
  */
 class Person {
-    constructor (name, age) {
-        this.name = name;
+    constructor (fname, lname, age) {
+        this.fname = fname;
+        this.lname = lname;
         this.age = age;
     }
-    getName () { return `name: ${this.name} age: ${this.age}` ; }
+    getName () {
+        let name = this.fname + ' ' + this.lname;
+        return `Name: ${name}  Age: ${this.age}`;
+    };
 };
 
-let peter = new Person('Peras', 45);
+let peter = new Person('Peras','Omondi', 45);
 console.log(peter.getName());
