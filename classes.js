05eb26edab337fd857c7892bf96bf7e2b26dd6ec
paullaster <paullasterokoth98@gmail.com>
2022-09-21 10:42:47 +0300

    encapsulating, accessors

--- classes.js
+++ classes.js
@@ -1,17 +1,24 @@
 /**
  * Classes
+ * properties(fields): Hold data for a class
+ * # - make the class properties private and 
+ * not accessible from outside classes
  */
 class Person {
+    #fname;
+    #lname;
+    #age;
     constructor (fname, lname, age) {
-        this.fname = fname;
-        this.lname = lname;
-        this.age = age;
+        this.#fname = fname;
+        this.#lname = lname;
+        this.#age = age;
     }
-    getName () {
-        let name = this.fname + ' ' + this.lname;
-        return `Name: ${name}  Age: ${this.age}`;
+    get name () {
+        let name = this.#fname + ' ' + this.#lname;
+        return `Name: ${name}  Age: ${this.#age}`;
     };
 };
 
 let peter = new Person('Peras','Omondi', 45);
-console.log(peter.getName());
+console.log(peter.name);
+//console.log(peter.getName());
