7676e2cf11a52fdf1b2dbd9a7f6c5c0b9a4a7f1b
paullaster <paullasterokoth98@gmail.com>
2022-09-21 11:04:10 +0300

    setters

--- classes.js
+++ classes.js
@@ -8,17 +8,28 @@ class Person {
     #fname;
     #lname;
     #age;
-    constructor (fname, lname, age) {
+    #favColor;
+    constructor (fname, lname, age, favcolor) {
         this.#fname = fname;
         this.#lname = lname;
         this.#age = age;
+        this.#favColor = favcolor;
     }
     get name () {
         let name = this.#fname + ' ' + this.#lname;
         return `Name: ${name}  Age: ${this.#age}`;
     };
+    
+    /**
+     * @param {any} color
+     */
+    set favoriteColor (color) {
+        this.#favColor = color;
+    }
 };
 
 let peter = new Person('Peras','Omondi', 45);
 console.log(peter.name);
+peter.favoriteColor = '#f00';
+
 //console.log(peter.getName());
