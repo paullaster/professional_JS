d2b39284f181b27b22d721360b8f19cd58d91dd7
paullaster <paullasterokoth98@gmail.com>
2022-09-24 07:34:03 +0300

    transforming string

--- array.methods.js
+++ array.methods.js
@@ -6,3 +6,9 @@
  * forEach method - for performing action on each element
  * in the array using a callback function
  */
+let arr = ['calvince', 'synthia', 'bridgid', 'idda', 'arthur', 'brenda'];
+
+function firstLetterToUpperCase (str) {
+    let transformedStr = str.charAt(0).toUpperCase() + str.slice(1);
+    console.log(transformedStr);
+}
