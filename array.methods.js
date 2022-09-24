d27454af681435def99f3eb4474efb2762287e5e
paullaster <paullasterokoth98@gmail.com>
2022-09-24 07:46:41 +0300

    .

--- array.methods.js
+++ array.methods.js
@@ -7,10 +7,20 @@
  * in the array using a callback function
  */
 let arr = ['calvince', 'synthia', 'bridgid', 'idda', 'arthur', 'brenda'];
+let arrNum = [[3,6],[8,9],[1,3,2]];
+
+Array.min = (arr) => {
+    return Math.min.apply (Math, arr);
+}
+console.log(Array.min(arrNum));
 
 function firstLetterToUpperCase (str) {
     let transformedStr = str.charAt(0).toUpperCase() + str.slice(1);
     console.log(transformedStr);
-}
+};
 //firstLetterToUpperCase('okoth')
 arr.forEach(firstLetterToUpperCase);
+
+arrNum.forEach( (el) => {
+    console.log(el);
+});
