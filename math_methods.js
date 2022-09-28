384924910154758af086bd58d0747c6017e82035
paullaster <paullasterokoth98@gmail.com>
2022-09-28 16:37:25 +0300

    max and min

--- math_methods.js
+++ math_methods.js
@@ -6,5 +6,9 @@ do {
     let random = Math.floor (Math.random() * 100) +10 ;
     arrayNum.push(random);
 }while (arrayNum.length < 11);
-let highest = Math.max (arrayNum);
-console.log(`Array: ${arrayNum}, \n highest value: ${highest}`);
+
+let i = 0;
+let highest = Math.max (45,23,456,786,746,342,55657,324,545467,34232,67678867,23234346,2324525,3346,1322344,56567);
+let lowest = Math.min (4898,5,3223434,4456,68769,32136,9087677,3434,2323234,45545,67868,890,6790,088,788,08989677,35335434,777777,0988888);
+
+console.log(`Array: ${arrayNum}, \n highest value: ${highest}, \n lowest value: ${lowest}`);
