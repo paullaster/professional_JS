4a2eb119cc4d9b9bc49390e52e1687f6daceaf87
paullaster <paullasterokoth98@gmail.com>
2022-09-28 11:11:14 +0300

    joininng string

--- string_methods.js
+++ string_methods.js
@@ -7,3 +7,12 @@
 let mySting = 'this is a string';
 let stringArray = mySting.split(' ');
 console.log(stringArray);
+
+
+
+/**
+ * Joining an array to strings
+ */
+
+let newarr = stringArray.join(' ');
+console.log (newarr);
