95c0820d1176c86f7e6e57042b2f61f4c7e4e4d9
paullaster <paullasterokoth98@gmail.com>
2022-09-28 12:54:17 +0300

    searching string

--- string_methods.js
+++ string_methods.js
@@ -15,4 +15,11 @@ console.log(stringArray);
  */
 
 let newarr = stringArray.join('-');
-console.log (newarr);
+//console.log (newarr);
+
+/**
+ * Working with index and position
+ */
+let randStr = ' this is a string, my girlfriend is a she Synthia, my name is haurweengs';
+let getIndexOf = randStr.indexOf ('is');
+console.log (getIndexOf);
