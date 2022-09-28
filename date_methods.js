17d7c1e12925ab75e2b6d6e8cd710d7f51bc7a38
paullaster <paullasterokoth98@gmail.com>
2022-09-28 17:48:48 +0300

    date setter and getters

--- date_methods.js
+++ date_methods.js
@@ -1,5 +1,7 @@
 /**
  * Wroking date and built-in date methods
  */
-let now = new Date("Wed Sep 05 2022 12:40:12 GMT+0200");
+let now = new Date(2022, 1, 10, 12, 10, 15, 100);
 console.log (now);
+
+//Method to get and set elements date
