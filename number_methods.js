ae82ecf3113067599a2e8ab698b1f65564afb0b3
paullaster <paullasterokoth98@gmail.com>
2022-09-28 16:11:27 +0300

    precision

--- number_methods.js
+++ number_methods.js
@@ -16,4 +16,5 @@ let hi2 = "hi";
 // Getting fixed number of decimals
 let dpNum = 0.786888547474565435352273788588558594949594549549594545949686866767655999459594949599666
 let capedNum = dpNum.toFixed(2);
-console.log ( capedNum );
+let nexCapedNum = dpNum.toPrecision(2);
+console.log ( capedNum, nexCapedNum );
