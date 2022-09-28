b5a230fae7ec6205886a2d6eba9f3fabb3a0c95b
paullaster <paullasterokoth98@gmail.com>
2022-09-28 17:42:21 +0300

    converting different strings to date

--- date_methods.js
+++ date_methods.js
@@ -1,5 +1,5 @@
 /**
  * Wroking date and built-in date methods
  */
-//let now = new Date();
-console.log (Date.now ());
+let now = new Date("Wed Sep 05 2022 12:40:12 GMT+0200");
+console.log (now);
