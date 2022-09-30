17c9e285ebf91714dbe9961a3e56c51f358b77d1
paullaster <paullasterokoth98@gmail.com>
2022-09-30 10:29:34 +0300

    DOM

--- bom_dom.js
+++ bom_dom.js
@@ -2,5 +2,10 @@
  * BOM:
  * DOM
  */
-console.dir (navigator.credentials.get);
+//console.dir (navigator.geolocation);
+//window.location.
+//console.dir (location.ancestorOrigins)
 history.go (-1);
+
+//DOM
+console.dir (document);
